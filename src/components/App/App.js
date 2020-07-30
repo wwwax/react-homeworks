8966f2575ee9cd8dafb46fd5ab447299915fa099
Max Ivanov <waxivanov@gmail.com>
2020-07-30 15:21:19 +0300

    upd

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App = () => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>App</h1>
+        </div>
+    );
+};
+
+export default App;
