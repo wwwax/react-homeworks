9a404804a69f09d7129a81c03be5e8b8d907b908
Max Ivanov <waxivanov@gmail.com>
2020-07-31 15:19:13 +0300

    upd

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -6,10 +6,27 @@ const Profile = ({ user }) => {
 
     return (
         <div className={styles.container}>
-            <img className={styles.avatar} src={user.avatar} alt='user avatar' />
-            <div>{user.name}</div>
-            <div>{`@${user.tag}`}</div>
-            <div>{user.location}</div>
+            <img className={styles.userAvatar} src={user.avatar} alt='user avatar' />
+            <div className={styles.userName}>{user.name}</div>
+            <div className={styles.userTag}>{`@${user.tag}`}</div>
+            <div className={styles.userLocation}>{user.location}</div>
+
+            <div className={styles.stats}>
+                <div className={styles.statsItem}>
+                    <div className={styles.statsName}>Followers</div>
+                    <div className={styles.statsQuantity}>{user.stats.followers}</div>
+                </div>
+
+                <div className={styles.statsItem}>
+                    <div className={styles.statsName}>Views</div>
+                    <div className={styles.statsQuantity}>{user.stats.views}</div>
+                </div>
+
+                <div className={styles.statsItem}>
+                    <div className={styles.statsName}>Likes</div>
+                    <div className={styles.statsQuantity}>{user.stats.likes}</div>
+                </div>
+            </div>
         </div>
     );
 };
