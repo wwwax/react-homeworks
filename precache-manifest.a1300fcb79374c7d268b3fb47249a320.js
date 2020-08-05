fb551b43690285617fe91ea13cce6d751b569444
Max Ivanov <waxivanov@gmail.com>
2020-08-05 10:13:49 +0300

    Updates

---
+++ precache-manifest.a1300fcb79374c7d268b3fb47249a320.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d307004b73f9356c7d801507620d407f",
+    "url": "/react-homework-01/index.html"
+  },
+  {
+    "revision": "8dbfda98b5044db8fedc",
+    "url": "/react-homework-01/static/css/main.82e7ec0d.chunk.css"
+  },
+  {
+    "revision": "2ec4c7e777ec23cc7532",
+    "url": "/react-homework-01/static/js/2.3f99af2f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-homework-01/static/js/2.3f99af2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dbfda98b5044db8fedc",
+    "url": "/react-homework-01/static/js/main.61a7f4bc.chunk.js"
+  },
+  {
+    "revision": "3aa4c0be60024fdaa9ac",
+    "url": "/react-homework-01/static/js/runtime-main.96e1776a.js"
+  }
+]);
